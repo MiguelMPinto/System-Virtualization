901723c576a4afef3ee118c3cc5edfea52ed0457
André Vaz <andre.vaz2005@gmail.com>
2025-11-19 17:31:13 +0000

    files added

---
+++ cw3/install-all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create users and groups
+
+# Copy files from the repository directory into system directories like:
+#    /etc/nginx/sites-available/
+#    /etc/systemd/system/
+#    /opt/isel/tvs/
+
+# Set proper permissions
+
+# Leave the system in a well known state
+
